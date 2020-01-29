70bfcb40684dc896ecea8b431ad61586d00dc596
Dan <daniel@loylap.com>
2020-01-29 11:04:13 +0000

    Fragment is not required here, crashing in a basic activity

--- MyApplication/app/src/main/java/com/sp/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/sp/myapplication/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.sp.myapplication
 
+import android.content.ComponentName
 import android.content.Context
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.os.RemoteException
 import androidx.appcompat.app.AppCompatActivity
+import co.poynt.api.model.Business
 import co.poynt.os.model.Intents
+import co.poynt.os.model.PoyntError
+import co.poynt.os.services.v1.IPoyntBusinessReadListener
+import co.poynt.os.services.v1.IPoyntBusinessService
 import org.tinylog.configuration.Configuration
 import org.tinylog.kotlin.Logger
 
 class MainActivity : AppCompatActivity() {
 
+    private var poyntBusinessService: IPoyntBusinessService? = null
+
+    val poyntBusinessServiceListener: IPoyntBusinessReadListener =
+        object : IPoyntBusinessReadListener.Stub() {
+            override fun onResponse(b: Business?, error: PoyntError?) {
+                //This line crashes the app if Logger.trace has not been called yet
+                Logger.trace("tinylog Logging Business response! " + b?.id)
+            }
+        }
+
+    private val businessConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
+            poyntBusinessService = IPoyntBusinessService.Stub.asInterface(service)
+
+            try {
+                poyntBusinessService?.getBusiness(poyntBusinessServiceListener)
+            } catch (e: RemoteException) {
+            }
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            poyntBusinessService = null
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Configuration.set("writer.file", filesDir.path + "/logs/tiny_log_{count}.txt")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val fragment = MainFragment()
-        supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+
+    override fun onResume() {
+        super.onResume()
+
+        //If this line is uncommented, tinylog does NOT crash
+        //Logger.trace("tinylog binding to business service")
+
+        bindService(
+            Intents.getComponentIntent(Intents.COMPONENT_POYNT_BUSINESS_SERVICE),
+            businessConnection, Context.BIND_AUTO_CREATE
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        unbindService(businessConnection)
     }
 }
